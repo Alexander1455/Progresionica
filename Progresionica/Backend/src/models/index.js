54b65b0e5369f688044b17c9dade45b5e9068014
Alexander1455 <alexander21111121@gmail.com>
2024-01-12 13:36:33 -0500

    Configure relationships: User,Routine, and Phrases

---
+++ Progresionica/Backend/src/models/index.js
@@ -0,0 +1,8 @@
+import Phrases from './Phrases.models.js'
+import User from './User.models.js'
+import Routine from './Routine.model.js'
+
+User.hasMany(Routine, { foreignKey: 'userId' })
+Routine.belongsTo(User, { foreignKey: 'userId' })
+
+export { Phrases, User, Routine }
