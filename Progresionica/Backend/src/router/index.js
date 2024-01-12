86f77bcb0a1191f6ab1b4c229743ef855f637f8d
Alexander1455 <alexander21111121@gmail.com>
2024-01-12 13:39:25 -0500

    Export routers from the index

---
+++ Progresionica/Backend/src/router/index.js
@@ -0,0 +1,4 @@
+export { default as authRouter } from './auth.router.js'
+export { default as phrasesRouter } from './phrases.router.js'
+export { default as routineRouter } from './routine.router.js'
+export { default as userRouter } from './user.router.js'
